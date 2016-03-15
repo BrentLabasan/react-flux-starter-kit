1884bb20aa094d61dbac17d8320b3910ac1f4315
Brent Labasan <BrentLabasan@gmail.com>
2016-03-15 10:27:22 -0700

    Building Applications with React and Flux 4.2

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="jumbotron">
+                <h1>h1</h1>
+                <p>p</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
